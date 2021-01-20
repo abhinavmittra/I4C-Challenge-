2720c6d82b2336922ea05b479e5825da31b209fd
Abhinav <abhinavmittra@rediffmail.com>
2021-01-21 04:09:44 +0530

    Template for Login added

--- angular/donate-cart-ui/src/app/app.module.ts
+++ angular/donate-cart-ui/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLoginComponent } from './app-login/app-login.component';
+import { AppRegisterComponent } from './app-register/app-register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLoginComponent,
+    AppRegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
