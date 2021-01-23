2eecb9aafe85ce49e3cd819ccdda1e289348cfef
Abhinav <abhinavmittra@rediffmail.com>
2021-01-23 18:08:47 +0530

    Fixed Admin Screen bugs

---
+++ angular/donate-cart-ui/src/app/admin/admin.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  public getNgoRegReqsUrl = "replace with endpoint here";
+  public adminActionForRegUrl = "replace with endpoint here"
+  public httpHeaders = new HttpHeaders({
+    'Content-Type' : 'application/json; charset=utf-8'
+    
+       }); 
+   
+  public headerOptions = {
+    headers: this.httpHeaders
+       };    
+  constructor(private httpClient: HttpClient) { }
+
+  approveNgo(){
+    return this.httpClient.post<any>(this.adminActionForRegUrl,{"action":"accept"});
+  }
+  rejectNgo(){
+    return this.httpClient.post<any>(this.adminActionForRegUrl,{"action":"reject"});
+  }
+
+  viewNgoRegReqs(){
+    return this.httpClient.get(this.getNgoRegReqsUrl);
+  }
+
+
+}
--- angular/donate-cart-ui/src/app/admin/admin/admin.component.ts
+++ angular/donate-cart-ui/src/app/admin/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DoneeReg } from '../donee-reg';
+import {AdminService} from '../admin.service';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -7,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private adminService:AdminService) { }
+  public ngoRegRequests : DoneeReg[] = [];
   ngOnInit(): void {
-    
+    //Make a get request to server to fetch all donee objects with pending registration.
+
+    //Demo variable to check out layout
+
+    var doneeObj = new DoneeReg("ngo 1","address 1","pincode 1","phone 1","panno 1","web1","email1");
+    this.ngoRegRequests.push(doneeObj);
+  }
+  approveRequest(index:number){
+    this.ngoRegRequests.splice(index,1);
+    this.adminService.approveNgo().subscribe();
+  }
+  rejectRequest(index:number){
+    this.ngoRegRequests.splice(index,1);
+    this.adminService.rejectNgo().subscribe();
   }
 
 }
